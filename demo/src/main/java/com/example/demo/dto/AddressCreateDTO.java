3e8d243ef90986c6f958b8bfad3e27935630f550
N!XU <nixpl00@gmail.com>
2025-07-03 15:02:48 +0200

    First commit Address requests done

---
+++ demo/src/main/java/com/example/demo/dto/AddressCreateDTO.java
@@ -0,0 +1,30 @@
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record AddressCreateDTO(
+        @NotBlank(message = "Address line 1 cannot be empty")
+        @Size(max = 50, message = "Address line 1 cannot exceed 255 characters")
+        String address,
+
+        @Size(max = 50, message = "Address line 2 cannot exceed 255 characters")
+        String address2,
+
+        @NotBlank(message = "District cannot be empty")
+        @Size(max = 20, message = "District cannot exceed 50 characters")
+        String district,
+
+        @NotNull(message = "City ID cannot be null")
+        @Size(min = 0, message = "CityID cannot be smaller than 0")
+        Integer cityId,
+
+        @NotBlank(message = "Postal code cannot be empty")
+        @Size(max = 10, message = "Postal code cannot exceed 10 characters")
+        String postalCode,
+
+        @NotBlank(message = "Phone number cannot be empty")
+        @Size(max = 20, message = "Phone number cannot exceed 20 characters")
+        String phone
+){}
---
+++ demo/src/main/java/com/example/demo/model/Address.java
@@ -0,0 +1,47 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Integer addressId;
+
+    @Column(name = "address", length = 50, nullable = false)
+    private String address;
+
+    @Column(name = "address2", length = 50)
+    private String address2;
+
+    @Column(name = "district", length = 20, nullable = false)
+    private String district;
+
+    @Column(name = "city_id", nullable = false)
+    private Integer cityId;
+
+    @Column(name = "postal_code", length = 10)
+    private String postalCode;
+
+    @Column(name = "phone", length = 20, nullable = false)
+    private String phone;
+
+    @Column(name = "last_update", nullable = false)
+    private Date lastUpdate;
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.lastUpdate = new Date();
+    }
+}
