a8129634c472ca1deeeb162f4909b641702b1c6c
Apiotr16st <apiotr16@wp.pl>
2025-07-07 13:50:59 +0200

    fix: unused imports

--- demo/src/main/java/com/example/demo/repository/AddressRepository.java
+++ demo/src/main/java/com/example/demo/repository/AddressRepository.java
@@ -1,12 +1,8 @@
 package com.example.demo.repository;
 
-import com.example.demo.dto.AddressDTO;
 import com.example.demo.model.Address;
 import com.example.demo.model.City;
-import com.example.demo.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
