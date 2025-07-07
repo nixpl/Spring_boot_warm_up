9e76bbb2d72c1120a3db0c51b02d7ec8239a3812
N!XU <nixpl00@gmail.com>
2025-07-07 14:44:04 +0200

    Address update null fix

--- demo/src/main/java/com/example/demo/dto/AddressCreateDTO.java
+++ demo/src/main/java/com/example/demo/dto/AddressCreateDTO.java
@@ -4,8 +4,6 @@ import jakarta.validation.constraints.*;
 
 public record AddressCreateDTO(
         @NotBlank(message = "Address line 1 cannot be empty")
-        @Pattern(regexp = "^\\d+\\s(?:[A-Z][a-z]*\\s)*[A-Z][a-z]*$",
-                message = "The address format is invalid. Expected format: 'Number Street_Name Street_Type")
         @Size(max = 50, message = "Address line 1 cannot exceed 50 characters")
         String address,
 
@@ -22,11 +20,9 @@ public record AddressCreateDTO(
 
         @NotBlank(message = "Postal code cannot be empty")
         @Size(max = 10, message = "Postal code cannot exceed 10 characters")
-        @Pattern(regexp = "^[0-9-]*$", message = "Postal code can only contain digits and hyphens, without spaces")
         String postalCode,
 
         @NotBlank(message = "Phone number cannot be empty")
         @Size(max = 20, message = "Phone number cannot exceed 20 characters")
-        @Pattern(regexp = "^\\+?\\d*$", message = "Phone number can only contain digits and an optional leading plus sign.")
         String phone
 ){}
